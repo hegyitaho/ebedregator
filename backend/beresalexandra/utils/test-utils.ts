e2c39397072b90f3496819ad9bdb751bb4953748
hegyitaho <hegyitaho@gmail.com>
2019-12-30 17:39:02 +0100

    fix test resource path

--- backend/beresalexandra/utils/test-utils.ts
+++ backend/beresalexandra/utils/test-utils.ts
@@ -2,7 +2,7 @@ import fs from 'fs'
 import {loadSite, menu} from '../converted-menu'
 
 export function loadBeresAlexandraTestDouble(): CheerioStatic {
-  const body = fs.readFileSync('test-resources/beresalexandra-aktualis-etlap.html')
+  const body = fs.readFileSync('../../../test-resources/beresalexandra-aktualis-etlap.html')
   return loadSite(body)
 }
 
