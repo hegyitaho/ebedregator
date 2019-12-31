d0c29802e5c4df8dba69cfb4f901116216e68c66
hegyitaho <hegyitaho@gmail.com>
2019-12-31 14:37:53 +0100

    put static assets in public dir

--- backend/beresalexandra/utils/test-utils.ts
+++ backend/beresalexandra/utils/test-utils.ts
@@ -1,20 +1,12 @@
 import fs from 'fs'
-import path from 'path'
 
 import {loadSite, menu} from '../converted-menu'
 
 export function loadBeresAlexandraTestDouble(): CheerioStatic {
-  const body = fs.readFileSync(dummyMenuPath)
+  const body = fs.readFileSync('beresalexandra-aktualis-etlap.html')
   return loadSite(body)
 }
 
 export function dummyFoodDataForAWeek() {
   return menu(loadBeresAlexandraTestDouble())
 }
-
-const dummyMenuPath = process.env.PROJECT_DIRNAME ?
-  path.join(process.env.PROJECT_DIRNAME, 'resources', 'beresalexandra-aktualis-etlap.html') :
-  'resources/beresalexandra-aktualis-etlap.html'
-
-console.log('resource path: ' + dummyMenuPath)
-console.log('process.env.PROJECT_DIRNAME: ' + process.env.PROJECT_DIRNAME)
--- next.config.js
+++ next.config.js
@@ -1,29 +1,10 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const CopyWebpackPlugin = require('copy-webpack-plugin')
-
-const path = require('path')
-const resources = path.join(__dirname, 'resources')
-
 module.exports = {
-  env: {
-    PROJECT_DIRNAME: __dirname,
-  },
   webpack: config => {
     // Fixes npm packages that depend on `fs` module
     config.node = {
       fs: 'empty',
     }
-    config.plugins.push(
-      new CopyWebpackPlugin([
-        {
-          from: 'resources',
-          to: resources,
-        },
-      ]),
-    )
     return config
-  },
-  functions: {
-    excludeFiles: ['**/utils/*'],
   },
 }
