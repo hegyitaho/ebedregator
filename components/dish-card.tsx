1c35b78de209f5f155d15ff060c65d55060b5ef0
hegyitaho <hegyitaho@gmail.com>
2020-01-24 17:30:17 +0100

    make dish cards more colorful and compact

--- components/dish-card.tsx
+++ components/dish-card.tsx
@@ -1,48 +1,32 @@
 import React from 'react'
-import {makeStyles, createStyles, Theme} from '@material-ui/core/styles'
-
-import {FoodData} from 'backend/domain'
+import {makeStyles, createStyles} from '@material-ui/core/styles'
 import Grid from '@material-ui/core/Grid'
 import Card from '@material-ui/core/Card'
-import {withStyles} from '@material-ui/core/styles'
-
 import CardContent from '@material-ui/core/CardContent'
+import Paper from '@material-ui/core/Paper'
+import {purple, lightBlue, teal, yellow} from '@material-ui/core/colors'
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      flex: 'column',
-    },
-    name: {
-      height: 140,
-      width: 100,
-    },
-    site: {
-      padding: theme.spacing(2),
-    },
-  }),
-)
+import {FoodData} from 'backend/domain'
+
 
 const DishCard = (props: {dish: FoodData}) => {
   const classes = useStyles(props)
   return (
-    <Grid item>
+    <Grid item className={classes.root}>
       <Card raised={true}>
         <CardContent>
           <h3 className="name">
             {props.dish.name}
           </h3>
-          <Grid container spacing={2}>
-            {['kcal', 'carbohydrate', 'fat', 'protein'].map(key => 
-              <Grid item key={key}>
-                <Card > 
-                  <CardContent>
-                    {`${key}: ${props.dish[key]}`}
-                  </CardContent>
-                </Card>
-              </Grid>,
-            )}
-          </Grid>
+          {/* <Grid container spacing={2}> */}
+          {['kcal', 'carbohydrate', 'fat', 'protein'].map(key => 
+          // <Grid  item key={key}>
+            <Paper key={key} variant="outlined" className={key}>
+              {`${key === 'carbohydrate' ? 'carb' : key}: ${props.dish[key]}`}
+            </Paper>,
+            // </Grid>,
+          )}
+          {/* </Grid> */}
         </CardContent>
       </Card>
     </Grid>
@@ -50,3 +34,34 @@ const DishCard = (props: {dish: FoodData}) => {
 }
 
 export default DishCard
+
+const useStyles = makeStyles((theme) =>
+  createStyles({ 
+    root: {
+      '& .kcal': {
+        backgroundColor: purple['A100'],
+        'padding': theme.spacing(0.5),
+        'margin-top': theme.spacing(0.5),
+        'margin-bottom': theme.spacing(0.5),
+      },
+      '& .carbohydrate': {
+        backgroundColor: lightBlue['A100'],
+        'padding': theme.spacing(0.5),
+        'margin-top': theme.spacing(0.5),
+        'margin-bottom': theme.spacing(0.5),
+      },
+      '& .fat': {
+        backgroundColor: teal['A100'],
+        'padding': theme.spacing(0.5),
+        'margin-top': theme.spacing(0.5),
+        'margin-bottom': theme.spacing(0.5),
+      },
+      '& .protein': {
+        backgroundColor: yellow['A100'],
+        'padding': theme.spacing(0.5),
+        'margin-top': theme.spacing(0.5),
+        'margin-bottom': theme.spacing(0.5),
+      },
+    },
+  }),
+)
